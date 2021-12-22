4b1f7e9938ea7cea6b811f34e2751dde5bdb3f6f
ThanhHoanghg <96186510+ThanhHoanghg@users.noreply.github.com>
2021-12-23 02:30:51 +0700

    Create btvn 9.cpp

---
+++ btvn/9 btvn/btvn 9.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <math.h>
+int nguyenTo(int n){
+
+for (int i=2;i<=n;i++)
+	if (n%i==0){
+		return true;
+		break;
+	}else{
+		return false;
+  	}
+}
+
+
+
+
+
+int tongCacChuSo(int n){
+	
+	int s=0
+	for(int i=0;i<=n;i++){
+		s=s+i;
+	}
+	return s;
+}
+
+
+
+
+
+int chuViVaDienTich(int a; int b; int c){
+	
+	if(a<b+c){
+		if(b<a+c){
+			if(c<b+a){
+				return true;
+				int chuvi, p, dientich;
+				chuvi=a+b+c return chuvi;
+				p=(float)(a+b+c)/2;
+				dientich=sqrt(p*(p-a)*(p-b)*(p-c)) return dientich;
+			}else{
+				return false;
+			}
+		}else{
+			return false;
+		}
+	}else{
+		return false;
+	}
+}
+
+
+
+
+
+int uocChungLonNhat(int a; intb){
+	
+	for(int i=a;i>=1;i--){
+		if(a%i==0 && b%i==0){
+			break;
+		} 
+	}
+	return i;
+}
+
+
+
+
+
+int boiChungNhoNhat(int a; intb){
+	
+	for(int i=0;i<=a*b;i++){
+		if(i%a==0 && i%b==0){
+			break;
+		}
+	}
+	return i;
+}
+
+
+
+
+
+
+
