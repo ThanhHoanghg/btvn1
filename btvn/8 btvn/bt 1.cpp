b9b4e9f1362316ce600211512b84e74cd501e010
ThanhHoanghg <96186510+ThanhHoanghg@users.noreply.github.com>
2021-12-23 00:20:40 +0700

    Create bt 1.cpp

---
+++ btvn/8 btvn/bt 1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("nhap mang =");
+	scanf("%d",&n);
+	int arr[n];
+	for(int i=0;i<n;i++){
+		printf("nhap so trong mang =");
+		scanf("%d",arr[i]);
+	}
+	for(int i=0; i<n-1; i++){
+		for(int j=0;j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+	int x;
+	printf(" nhap x=");
+	scanf("%d",&x);
+	for(int i=temp;i>0;i--){
+		if(arr[i]<=x){
+			printf(" so nho hon va gan x nhat la : %d ",arr[i]);
+			break;
+		}
+	}
+}
+
