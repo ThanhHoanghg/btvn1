ddd8f95ffa1c32b294b7f766f279f81f21f9a0aa
ThanhHoanghg <96186510+ThanhHoanghg@users.noreply.github.com>
2021-12-22 10:35:06 +0700

    Update bt 2.cpp

--- btvn/5 lab/bt 2.cpp
+++ btvn/5 lab/bt 2.cpp
@@ -9,13 +9,15 @@ int main(){
 	}while(a<=0,b<=0);
 	
 	for(int uc = a;uc>=1,b;uc--){
-		if(a%uc==0,b%uc==0){
+		if(a%uc==0 && b%uc==0){
 			printf("uoc chung lon nhat la : %d\n",uc);
+			break;
 		}
 	}
 	for(int BC = a;BC<=a*b;BC++){
-		if(BC%a==0,BC%b==0){
-			printf("boi chung lon nhat la : %d\n",BC);
+		if(BC%a==0 && BC%b==0){
+			printf("boi chung nho nhat la : %d\n",BC);
+			break;
 		}
 	} 	 
 }
